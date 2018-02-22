c1fdcc95f9d4aebd5418a427f0b00ac99ad4a328
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-02-22 04:39:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,8 @@
 # Your code goes here!
-class Dog
-  def name=(dogs_name)
-    @name = dogs_name
-  end 
-
-  def name
-    @name
-  end 
+class dog 
+  def 
   
-end 
-
-class Dog
-  def bark
-    puts "woof!"
-  end 
+  
+  
   
 end 
